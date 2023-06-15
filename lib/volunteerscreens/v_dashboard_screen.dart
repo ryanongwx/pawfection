7d3a6109fc12f9d45333fed5f0aabc2c01f23c65
ryanongwx <ryanongwx@gmail.com>
2023-06-15 10:22:47 +0800

    Add User Complete Task Button

--- lib/volunteerscreens/v_dashboard_screen.dart
+++ lib/volunteerscreens/v_dashboard_screen.dart
@@ -1,13 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as FirebaseAuth;
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_segment/flutter_advanced_segment.dart';
-import 'package:pawfection/models/pet.dart';
 import 'package:pawfection/models/task.dart';
-import 'package:pawfection/models/user.dart';
-import 'package:pawfection/repository/storage_repository.dart';
 import 'package:pawfection/repository/task_repository.dart';
 import 'package:pawfection/service/task_service.dart';
 import 'package:searchable_listview/searchable_listview.dart';
+import 'package:pawfection/volunteerscreens/v_task_dialog.dart' as taskDialog;
+import 'package:pawfection/managerscreens/m_volunteer_dialog.dart'
+    as volunteerDialog;
 
 class VDashboardScreen extends StatefulWidget {
   const VDashboardScreen({super.key});
@@ -20,6 +21,8 @@ final _selectedSegment_04 = ValueNotifier('Pending');
 
 final taskRepository = TaskRepository();
 final taskService = TaskService();
+
+final _auth = FirebaseAuth.FirebaseAuth.instance; // authInstance
 
 // List<Task> taskList = [];
 
@@ -52,82 +55,98 @@ class _VDashboardScreenState extends State<VDashboardScreen> {
           }
 
           return Scaffold(
-              appBar: AppBar(title: const Text('Dashboard')),
+              appBar: AppBar(
+                title: const Text('Tasks'),
+              ),
               body: Stack(children: [
-                Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      child: AdvancedSegment(
-                        controller: _selectedSegment_04,
-                        segments: const {
-                          'Pending': 'Pending',
-                          'Completed': 'Completed',
-                          'Open': 'Open',
-                        },
-                        activeStyle: const TextStyle(
-                          // TextStyle
-                          color: Colors.black,
-                          fontWeight: FontWeight.w600,
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.7,
+                  child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: AdvancedSegment(
+                          controller: _selectedSegment_04,
+                          segments: const {
+                            'Pending': 'Pending',
+                            'Completed': 'Completed',
+                            'Open': 'Open',
+                          },
+                          activeStyle: const TextStyle(
+                            // TextStyle
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          inactiveStyle: const TextStyle(
+                            // TextStyle
+                            color: Colors.white54,
+                          ),
+                          backgroundColor: Colors.black26,
+                          // Color
+                          sliderColor: Colors.white,
+                          // Color
+                          sliderOffset: 2.0,
+                          // Double
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8.0)),
+                          // BorderRadius
+                          itemPadding: const EdgeInsets.symmetric(
+                            // EdgeInsets
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          animationDuration:
+                              const Duration(milliseconds: 250), // Duration
                         ),
-                        inactiveStyle: const TextStyle(
-                          // TextStyle
-                          color: Colors.white54,
-                        ),
-                        backgroundColor: Colors.black26,
-                        // Color
-                        sliderColor: Colors.white,
-                        // Color
-                        sliderOffset: 2.0,
-                        // Double
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(8.0)),
-                        // BorderRadius
-                        itemPadding: const EdgeInsets.symmetric(
-                          // EdgeInsets
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        animationDuration:
-                            const Duration(milliseconds: 250), // Duration
-                      ),
-                    )),
+                      )),
+                ),
                 ValueListenableBuilder(
                   valueListenable: _selectedSegment_04,
                   builder: (context, value, child) {
                     return Padding(
-                      padding:
-                          const EdgeInsets.only(top: 75.0, left: 20, right: 20),
-                      child: SearchableList<Task>(
-                        autoFocusOnSearch: false,
-                        initialList: taskList
-                            .where((element) => element.status
-                                .contains(_selectedSegment_04.value))
-                            .toList(),
-                        builder: (Task task) => TaskItem(task: task),
-                        filter: (value) => taskList
-                            .where((element) => element.name
-                                .toLowerCase()
-                                .contains(value.toLowerCase()))
-                            .where((element) => element.status
-                                .contains(_selectedSegment_04.value))
-                            .toList(),
-                        emptyWidget: const EmptyView(),
-                        inputDecoration: InputDecoration(
-                          labelText: "Search Task",
-                          fillColor: Colors.white,
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Colors.blue,
-                              width: 1.0,
-                            ),
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                      ),
-                    );
+                        padding: const EdgeInsets.only(
+                            top: 75.0, left: 20, right: 20),
+                        child: SearchableList<Task>(
+                            autoFocusOnSearch: false,
+                            initialList: taskList
+                                .where((element) => element.status
+                                    .contains(_selectedSegment_04.value))
+                                .toList(),
+                            builder: (Task task) => TaskItem(task: task),
+                            filter: (value) => taskList
+                                .where((element) => element.name
+                                    .toLowerCase()
+                                    .contains(value.toLowerCase()))
+                                .where((element) => element.status
+                                    .contains(_selectedSegment_04.value))
+                                .toList(),
+                            emptyWidget: const EmptyView(),
+                            inputDecoration: const InputDecoration(
+                              labelText: "Search Task",
+                              fillColor: Colors.white,
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.blue,
+                                  width: 1.0,
+                                ),
+                              ),
+                            )));
                   },
                 ),
+                // Padding(
+                //     padding: EdgeInsets.only(bottom: 100),
+                //     child: Align(
+                //       alignment: Alignment.bottomCenter,
+                //       child: ElevatedButton(
+                //           onPressed: () {
+                //             Navigator.push(
+                //               context,
+                //               MaterialPageRoute(
+                //                   builder: (context) => MCreateTaskScreen()),
+                //             );
+                //           },
+                //           child: Text('Create Tasks')),
+                //     )),
               ]));
         });
   }
@@ -143,43 +162,70 @@ class TaskItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 60,
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
-          children: [
-            const SizedBox(
-              width: 10,
+    if (task == null) {
+      return const Column();
+    } else {
+      return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(width: 2),
             ),
-            const Icon(
-              Icons.account_circle,
-              color: Colors.black,
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '${task.name}',
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
+            child: InkWell(
+              onTap: () {
+                if (task.referenceId != null) {
+                  taskDialog.displayTaskItemDialog(context, task.referenceId!);
+                }
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.account_circle,
+                      color: Colors.black,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          task.name,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    // To push icon to the right
+                    const Expanded(child: SizedBox()),
+                    if (task.status == "Open")
+                      SizedBox(
+                        height: 24.0, // Change as needed
+                        width: 24.0, // Change as needed
+                        child: IconButton(
+                          padding: EdgeInsets.zero, // removes default padding
+                          alignment: Alignment.center, // centers the icon
+                          icon: const Icon(Icons.person_add),
+                          iconSize: 20.0, // Change as needed
+                          onPressed: () async {
+                            volunteerDialog.displayVolunteersDialog(
+                                context, task);
+                          },
+                        ),
+                      )
+                  ],
                 ),
-              ],
+              ),
             ),
-          ],
-        ),
-      ),
-    );
+          ));
+    }
   }
 }
 
