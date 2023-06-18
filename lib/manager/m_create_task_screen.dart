da0d2210ff1bd27e614fd716513a556d04ff53ab
junnengsoo <soojunneng01@gmail.com>
2023-06-18 12:03:52 +0800

    Delete showdialog function

--- lib/manager/m_create_task_screen.dart
+++ lib/manager/m_create_task_screen.dart
@@ -36,27 +36,6 @@ class _MCreateTaskScreenState extends State<MCreateTaskScreen> {
 
   final _auth = FirebaseAuth.FirebaseAuth.instance; // authInstance
 
-  void _showDialog(Widget child) {
-    showCupertinoModalPopup<void>(
-      context: context,
-      builder: (BuildContext context) => Container(
-        height: 216,
-        padding: const EdgeInsets.only(top: 6.0),
-        // The Bottom margin is provided to align the popup above the system navigation bar.
-        margin: EdgeInsets.only(
-          bottom: MediaQuery.of(context).viewInsets.bottom,
-        ),
-        // Provide a background color for the popup.
-        color: CupertinoColors.systemBackground.resolveFrom(context),
-        // Use a SafeArea widget to avoid system overlaps.
-        child: SafeArea(
-          top: false,
-          child: child,
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     if (Platform.isAndroid) {
