a432af7e22797158d1fef9da66f1de93d68dfea1
junnengsoo <soojunneng01@gmail.com>
2023-06-25 16:43:31 +0800

    Set up addPetRepo test

--- test/repository/pet_repository_test.dart
+++ test/repository/pet_repository_test.dart
@@ -1,11 +1,37 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:pawfection/repository/pet_repository.dart';
 
 void main() {
-  test('Given a JSON file of aa pet When passed into addPetRepo Then the pet is stored in Firestore', () async {
-    // ARRANGE
-    final instance = FakeFirebaseFirestore();
-    // ACT
-    // ASSERT
+  late PetRepository petRepo;
+  late FakeFirebaseFirestore instance;
+
+  // Sets up a new FakeFirebaseFirestore instance for each test
+  setUp(() {
+    instance = FakeFirebaseFirestore();
+    petRepo = PetRepository(instance);
+  });
+
+  // Tears down the instance at the end of each test
+  tearDown(() {
+    instance = FakeFirebaseFirestore();
+  });
+
+  test('addPetRepo', () async {
+    // Arrange
+    final pet = {
+      'name': 'Catto',
+      'breed': 'Persian',
+      'description': 'A nice cat',
+      'profilepicture': 'assets/images/user_profile.png'
+    };
+
+    // Act
+    await petRepo.addPetRepo(pet);
+
+    // Assert
+    final snapshot = await instance.collection('pets').get();
+    expect(snapshot.docs.length, 1);
+    expect(snapshot.docs.first.get('name'), 'Catto');
   });
 }
